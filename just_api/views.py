2364916e58776ae33d9ecb1a03a230a75dd89c33
KingAnointing <adetoyeanointing15@gmail.com>
2022-03-26 21:46:26 -0400

    create API post method

--- just_api/views.py
+++ just_api/views.py
@@ -5,6 +5,9 @@ from rest_framework import generics
 from rest_framework import mixins
 
 # Create your views here.
-class GenericApiView(generics.GenericAPIView, mixins.ListModelMixin):
+class GenericApiView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin,):
     def get(self, request):
         return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
