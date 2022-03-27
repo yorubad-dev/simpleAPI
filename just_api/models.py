501f587fa919566533a903d4826bca210dcb138d
KingAnointing <adetoyeanointing15@gmail.com>
2022-03-26 21:21:39 -0400

    created url file in app and article class in model

--- just_api/models.py
+++ just_api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=40)
+    author = models.CharField(max_length=35)
+    article_body = models.CharField(max_length=1500)
+    email = models.EmailField(max_length=100)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
