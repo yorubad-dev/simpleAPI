1e10140bbfbb806d6a4d5f85e6766fa105bfe153
KingAnointing <adetoyeanointing15@gmail.com>
2022-03-26 21:38:59 -0400

    insert "rest_framework" in installed app, import generic and mixin

--- just_api/views.py
+++ just_api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Article
 from .serializer import ArticleSerializer
+from rest_framework import generics
+from rest_framework import mixins
 
 # Create your views here.
