a4ba5b5fe56eed2c3a0de311fff877bd5c5b9b24
KingAnointing <adetoyeanointing15@gmail.com>
2022-03-27 22:48:53 -0400

    added api id field and lookup field

--- just_api/views.py
+++ just_api/views.py
@@ -8,18 +8,24 @@ from rest_framework import mixins
 
 
 class GenericApiView(generics.GenericAPIView, mixins.ListModelMixin,
-                     mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+                     mixins.CreateModelMixin, mixins.UpdateModelMixin,
+                     mixins.DestroyModelMixin, mixins.RetrieveModelMixin):
     serializer_class = ArticleSerializer
     queryset = Article.objects.all()
+    lookup_field = 'id'
 
-    def get(self, request):
-        return self.list(request)
 
-    def post(self, request):
+    def get(self, request, id=None):
+        if id:
+            return self.retrieve(request)
+        else:
+            return self.list(request)
+
+    def post(self, request, id=None):
         return self.create(request)
 
-    def put(self, request):
+    def put(self, request, id=None):
         return self.update(request)
 
-    def delete(self, request):
+    def delete(self, request, id=None):
         return self.destroy(request)
