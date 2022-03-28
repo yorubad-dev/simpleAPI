96ed7a5b3ad4851139ab9eafe7d0b064405c40e0
KingAnointing <adetoyeanointing15@gmail.com>
2022-03-27 22:26:56 -0400

    added serializer_class and queryset

--- just_api/views.py
+++ just_api/views.py
@@ -5,8 +5,13 @@ from rest_framework import generics
 from rest_framework import mixins
 
 # Create your views here.
+
+
 class GenericApiView(generics.GenericAPIView, mixins.ListModelMixin,
-    mixins.CreateModelMixin,mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+                     mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+    serializer_class = ArticleSerializer
+    queryset = Article
+
     def get(self, request):
         return self.list(request)
 
@@ -14,7 +19,7 @@ class GenericApiView(generics.GenericAPIView, mixins.ListModelMixin,
         return self.create(request)
 
     def put(self, request):
-        return self.update(request)   
+        return self.update(request)
 
     def delete(self, request):
-        return self.destroy(request) 
+        return self.destroy(request)
