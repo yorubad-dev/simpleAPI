cca0c890bf20a275a3aa984c0378fbc183528e49
KingAnointing <adetoyeanointing15@gmail.com>
2022-03-27 22:51:39 -0400

    added API id path to urls

--- just_api/urls.py
+++ just_api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 
 from . import views
 urlpatterns = [
-    path('article/', views.GenericApiView.as_view())
+    path('article/', views.GenericApiView.as_view()),
+    path('article/<int:id>/', views.GenericApiView.as_view()),
 ]
